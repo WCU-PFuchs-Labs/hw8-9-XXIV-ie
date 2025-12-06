cdc5ab4dd02017913bc8ae4bbe2f9356d7fba87a
Francine Lohose <flohose503@wcupa.edu>
2025-12-06 11:14:34 -0800

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -1,5 +1,3 @@
-package Binary;
-
 public class Variable extends Unop {
     private int index;
 
@@ -14,6 +12,6 @@ public class Variable extends Unop {
 
     @Override
     public String toString() {
-        return "X" + index;
+        return "x" + index;
     }
 }
