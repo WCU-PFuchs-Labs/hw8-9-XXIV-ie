24fcf270ec96a362340f50056fd9d2ef64e9c32d
Francine Lohose <flohose503@wcupa.edu>
2025-12-06 11:12:09 -0800

    Update NodeFactory.java

--- NodeFactory.java
+++ NodeFactory.java
@@ -1,42 +1,36 @@
-package Binary;
-
 import java.util.Random;
 
 public class NodeFactory {
-    private final int numIndepVars;
-    private final Binop[] currentOps;
+    private Binop[] currentOps;
+    private int numIndepVars;
 
-    //Constructor that accepts Binop and Int numIndepVars
-    public NodeFactory(Binop[] b, int numVars) {
-        if (b == null || b.length == 0) {
-            throw new IllegalArgumentException("The given Binop array is empty");
-        }
-        if (numVars < 0) {
-            throw new IllegalArgumentException("The given number of variables is negative");
-        }
-        currentOps = b;
-        numIndepVars = numVars;
+    public NodeFactory(Binop [] binops, int numVars) {
+        this.numIndepVars = numVars;
+        this.currentOps = binops;
+
     }
+
+    public Node getOperator (Random rand) {
+        int index = rand.nextInt(currentOps.length);
+        Binop clonedOp = (Binop) currentOps[index].clone();
+        return new Node(clonedOp);
+    }
+
+    public Node getTerminal (Random rand) {
+        int choice = rand.nextInt(numIndepVars + 1);
+
+        if (choice < numIndepVars) {
+            return new Node(new Variable(choice));
+        } else {
+            return new Node(new Const(rand.nextDouble()));
+        }
+    }
+
     public int getNumOps() {
         return currentOps.length;
     }
+
     public int getNumIndepVars() {
         return numIndepVars;
     }
-    //Method getOperator (Random rand)
-    public Node getOperator(Random rand) {
-        int i = rand.nextInt(currentOps.length);
-        Op op = (Op) currentOps [i].clone();
-        return new Node(op);
-    }
-    //Method getTerminal (Random rand)
-    public Node getTerminal(Random rand) {
-    int r = rand.nextInt(numIndepVars +1);
-    if (r < numIndepVars) {
-        return new Node(new Variable(r));
-    }else {
-        return new Node(new Const(rand.nextDouble()));
-        }
-    }
 }
-
