cbf981af77053bd7063291dad563f14ddc2125a7
Francine Lohose <flohose503@wcupa.edu>
2025-12-06 11:07:29 -0800

    Update Const.java

--- Const.java
+++ Const.java
@@ -1,27 +1,17 @@
-package Binary;
-
-/**
- * Code Template Author: David G. Cooper
- * Purpose: An operation class representing a constant number
- */
 public class Const extends Unop {
-    /** the constant value */
     private double value;
-    /**
-     * @param d the value to set the constant to.
-     */
-    public Const(double d) {
-        value = d;
+
+    public Const(double value) {
+        this.value = value;
     }
-    /**
-     * @return the value of the constant.
-     */
+
     @Override
-    public double eval(double [] values) {
+    public double eval(double[] values) {
         return value;
     }
+
     @Override
     public String toString() {
-        return String.format("%.2f", value);
+        return String.valueOf(value);
     }
 }
